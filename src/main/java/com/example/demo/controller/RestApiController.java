19ae0a1fb664f19c3410e25a69f80bf56dd9ec7d
Itsmanjitsharma <Itsmanjitsharma@gmail.com>
2024-01-30 10:44:53 +0530

    Commit to add new method

--- src/main/java/com/example/demo/controller/RestApiController.java
+++ src/main/java/com/example/demo/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class RestApiController {
     @GetMapping("/greet")
     public String  getHelloWorld(){
         return "Hello World";
+    }
+    @GetMapping("/greet/{name}")
+    public static String getGreet(@PathVariable("name") String name){
+       return "Hello "+name;
     }
 }
